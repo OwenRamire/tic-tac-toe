deacbc45e58b99da745266e46b4f48be17ac8c1e
owenramire <owenomar29@gmail.com>
2021-08-02 15:59:29 -0500

    Set players data
    Create a static method to set the player's data (name and the character
    that he chose).
    
    Also I made the printBoard() method in private because the other clases
    that I will create or that exist, doesn't care about this method

--- src/com/oramirez/Main.java
+++ src/com/oramirez/Main.java
@@ -1,14 +1,29 @@
 package com.oramirez;
 
+import java.util.Scanner;
+
+import com.oramirez.model.Player;
+
 public class Main {
     public static void main(String[] args) {
         char[][] mainBoard = new char[3][3];
         System.out.println("welcome to tic tac toe".toUpperCase());
         System.out.println("\tby Owen Ramirez\n");
         printBoard(mainBoard);
+
+        Player playerOne = new Player();
+        Player playerTwo = new Player();
+        byte auxPlayer = 1;
+
+        // Player one data
+        setPlayerData(playerOne, playerTwo, auxPlayer);
+        auxPlayer++;
+        // Player two data
+        setPlayerData(playerTwo, playerOne, auxPlayer);
+
     }
 
-    public static void printBoard(char[][] board) {
+    private static void printBoard(char[][] board) {
         System.out.print("+" + '\t' + "-" +  '\t' + "+" + '\t' + "-" + '\t' + "+" + '\t' + "-" + '\t' + "+");
         System.out.println();
         for(byte row = 0; row < 3; row++) {
@@ -21,4 +36,15 @@ public class Main {
             System.out.println();
         }
     }
+     private static void setPlayerData(Player playerOne, Player playerTwo, byte auxPlayer) {
+
+        System.out.print("\nEnter the player " + auxPlayer + " name: ");
+        Scanner scannerPlayerOneName = new Scanner(System.in);
+        playerOne.setPlayerName(scannerPlayerOneName.nextLine());
+        do {
+            System.out.print("Enter the player " + auxPlayer + " character: ");
+            Scanner scannerPlayerOneChar = new Scanner(System.in);
+            playerOne.setPlayerChar(scannerPlayerOneChar.next().charAt(0));
+        } while(!playerOne.isCharacterValid(playerOne.getPlayerChar(), playerTwo.getPlayerChar()));
+     }
 }
