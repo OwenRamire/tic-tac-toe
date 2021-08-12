ab62b82e950ed81fdae80a83527a8b4c335f2b4e
owenramire <owenomar29@gmail.com>
2021-08-12 11:02:58 -0500

    Fill board with players' char
    To do this feature, I create two methods:
    - fillBoard(): the purpose of this method is to fill the board with the
      position where the character will be.
    - startGame(): this method is to start the game, at this only this
      method only do a loop asking to the current player where he wants to
      put his character

--- src/com/oramirez/Main.java
+++ src/com/oramirez/Main.java
@@ -21,6 +21,7 @@ public class Main {
         // Player two data
         setPlayerData(playerTwo, playerOne, auxPlayer);
 
+        startGame(playerOne, playerTwo, mainBoard);
     }
 
     private static void printBoard(char[][] board) {
@@ -36,6 +37,7 @@ public class Main {
             System.out.println();
         }
     }
+
      private static void setPlayerData(Player playerOne, Player playerTwo, byte auxPlayer) {
 
         System.out.print("\nEnter the player " + auxPlayer + " name: ");
@@ -47,4 +49,76 @@ public class Main {
             playerOne.setPlayerChar(scannerPlayerOneChar.next().charAt(0));
         } while(!playerOne.isCharacterValid(playerOne.getPlayerChar(), playerTwo.getPlayerChar()));
      }
+
+     private static void startGame(Player p1, Player p2, char[][] board) {
+         char position;
+         byte i = 1;
+
+         System.out.println("\nReady!!!!!");
+         System.out.println("Go\n");
+         while(i <=9) {
+             if(i % 2 != 0) {
+                 System.out.print("put your position " + p1.getPlayerName() + ": ");
+                 Scanner scanner = new Scanner(System.in);
+                 position = scanner.next().charAt(0);
+                 fillBoard(position, p1.getPlayerChar(), board );
+             } else {
+                 System.out.print("put your position " + p2.getPlayerName() + ": ");
+                 Scanner scanner = new Scanner(System.in);
+                 position = scanner.next().charAt(0);
+                 fillBoard(position, p2.getPlayerChar(), board );
+             }
+             printBoard(board);
+             System.out.println();
+             i++;
+             position = 0;
+         }
+     }
+
+    private static void fillBoard(char position, char item, char[][] board){
+        switch(position){
+            case '1':
+            case '2':
+            case '3':
+                switch(position) {
+                    case '1':
+                        board[0][0] = item;
+                        break;
+                    case '2':
+                        board[0][1] = item;
+                        break;
+                    case '3':
+                        board[0][2] = item;
+                        break;
+                }
+            case '4':
+            case '5':
+            case '6':
+                switch(position) {
+                    case '4':
+                        board[1][0] = item;
+                        break;
+                    case '5':
+                        board[1][1] = item;
+                        break;
+                    case '6':
+                        board[1][2] = item;
+                        break;
+                }
+            case '7':
+            case '8':
+            case '9':
+                switch(position) {
+                    case '7':
+                        board[2][0] = item;
+                        break;
+                    case '8':
+                        board[2][1] = item;
+                        break;
+                    case '9':
+                        board[2][2] = item;
+                        break;
+                }
+        }
+    }
 }
